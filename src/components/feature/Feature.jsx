8eca2038be8469e42d4e253e7eeb351633a78c1d
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-11-16 01:16:05 +0530

    Features and Brand

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './feature.css'
+import React from "react";
+import "./feature.css";
 
-const Feature = () => {
-  return (
-    <div>Feature</div>
-  )
-}
+const Feature = ({ heading, para }) => {
+  return <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div/>
+      <h1>{heading}</h1>
+    </div>
+    <div className="gpt3__features-container__feature-text">
+      <p>{para}</p>
+    </div>
+  </div>;
+};
 
-export default Feature
+export default Feature;
--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './features.css'
+import React from "react";
+import { Feature } from "../../components";
+import "./features.css";
+
+const featuresDataArray = [
+  {
+    heading: "Improving end distrusts instantly ",
+    para: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+  {
+    heading: "Become the tended active",
+    para: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.",
+  },
+  {
+    heading: "Message or am nothing",
+    para: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+  },
+  {
+    heading: "Really boy law county",
+    para: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.",
+  },
+];
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresDataArray.map((feature, index) => {
+          return <Feature key={feature.heading + index} heading={feature.heading} para={feature.para} />;
+        })} 
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
